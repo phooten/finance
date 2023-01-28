061731e828d322f57cbb4f84122507e011588c15
phooten <parkerhooten@gmail.com>
2023-01-27 22:46:43 -0700

    Get input file and output a list. Added files to work with environmental variables once I get to that point and figure it out.

---
+++ setup.py
@@ -0,0 +1,12 @@
+# INWoRKS. DOESNOT FUNCTION.
+
+# This file is meant to set global variables in this repository. 
+
+
+def main():
+    ENV_FILE=".env"
+    exec(open( ENV_FILE ).read())
+
+
+if name="__main__":
+    main()
---
+++ tools/options-value-scan/options-value-scan.py
@@ -0,0 +1,63 @@
+#
+#
+#
+#
+#
+#
+import os
+
+
+
+# Retrieves a list of stocks from a text file and returns it as a list
+def getInput():
+    # TODO: Hard coding needs to be replaced with environment variables
+
+    # Paths / locations 
+    REPO_PATH = "/Users/phoot/bin/code/trading"
+    THIS_PATH = str(REPO_PATH) + "/tools/options-value-scan"
+    THIS_NAME = os.path.basename(__file__)
+    INPUT_NAME="list-1.txt"
+    INPUT_PATH= str(REPO_PATH) + "/input/" + str( INPUT_NAME )
+
+
+    # Take in input, put it into a list
+    print( "\n\n" )
+    if os.path.isfile( INPUT_PATH ):
+        print( "'" + str(INPUT_PATH) + "' was selected." )
+    else:
+        print( "'" + str( INPUT_PATH ) + "' does not exist. Exiting." )
+        exit()
+    print("\n\n")
+
+    file = open( INPUT_PATH, mode = 'r', encoding = 'utf-8-sig')
+    stock_list = file.readlines()
+    file.close()
+
+    stocks = []
+    for entry in stock_list:
+        # TODO: function to check stocks for validity
+        stocks.append( entry.replace("\n", "") )
+
+    return stocks
+
+
+
+
+# Driver of the code
+def main():
+    stocks = getInput()
+    print( stocks )
+
+
+
+
+    return
+
+
+
+
+if __name__=="__main__":
+    main()
+
+
+
