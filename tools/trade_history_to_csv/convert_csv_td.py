9810b9ca8f45cd4a1b15c1c7c8adc282e7fbab50
Parker Hooten <phooten@g.clemson.edu>
2023-09-14 10:05:22 -0600

    Additional csv's for unit test usage. Starting to write unit test for csv contents. Commiting bc my laptop is about to die

--- tools/trade_history_to_csv/convert_csv_td.py
+++ tools/trade_history_to_csv/convert_csv_td.py
@@ -247,6 +247,12 @@ def initialCsvFileCheck( csv_path ):
 
 
 def contentsCsvFileCheck( csv_path ):
+    """
+    Description:    Checks the input CSV for basic contents to make sure it can be parsed. Includes header contents,
+                    counts for columns and count for rows.
+    Arguments:      String - Path to a a CSV to be parsed
+    Returns:        Boolean - True for success, False for failure
+    """
 
     # TD Ameritrade Column Names
     td_headers = [  'DATE',
@@ -268,12 +274,16 @@ def contentsCsvFileCheck( csv_path ):
     row_count, col_count = df.shape
     column_names = list(df.columns)
 
+    # Checks the amount of rows in the given csv
+    row_minimum = 3
+    if row_count < row_minimum:
+        msgPrint( "File has [" + str(row_count) + "] rows. Need to have at least [" + str(row_minimum) + "].", "error", "TODO" )
+        return False
 
-    # Checks the amount of columns in given Csv
+    # Checks the amount of columns in given csv
     if col_count != len( td_headers ):
         msgPrint( "File has [" + str(col_count) + "] headers but expects [" + str( len( td_headers ) ) + "].", "error", "TODO" )
         return False
-
 
     # Checks the names of the headers in Csv
     for name in column_names:
@@ -288,15 +298,17 @@ def contentsCsvFileCheck( csv_path ):
     return True
 
 
-'''
-Name:   msgPrint
-Args:   msg:        [ string ] descriptive message to print out to user
-        choice:     [ string ] type of message being sent
-        func_name:  [ funct. ] function to get the function name
-Return: void
-Description:    Prints out messages to the user
-'''
 def msgPrint( msg, choice, func_name ):
+    """
+    Description:    Prints out message to the user
+    Arguments:      msg     - (string) to be printed out to user
+                    choice  - (string) defines which type of message is used.
+                              i.e. general system, error, etc.
+                    func_name - (function) function called that figures out what the 
+                            name of the previous funcion is
+    Returns:        Void
+    """
+
     if choice == "error":
         print( "\n" )
         print( "--------------------" )
--- tools/trade_history_to_csv/test_convert_csv_td.py
+++ tools/trade_history_to_csv/test_convert_csv_td.py
@@ -5,25 +5,26 @@ class TestInputCsvName( unittest.TestCase ):
 
     def test_incorrect_file_extension( self ):
         # File doesn't have .csv
-        self.assertEqual( convert_csv_td.initialCsvFileCheck("transactions_2022.cs"), False )
-        self.assertEqual( convert_csv_td.initialCsvFileCheck("transactions_2022csv"), False )
-        self.assertEqual( convert_csv_td.initialCsvFileCheck("transactions_2022.csvs"), False )
-        self.assertEqual( convert_csv_td.initialCsvFileCheck("transactions.csv_2022"), False )
-        self.assertEqual( convert_csv_td.initialCsvFileCheck("../../sensitive_files/transactions_2022.csv"), True )
+        self.assertEqual( convert_csv_td.initialCsvFileCheck("unittest/transactions_2022.cs"), False )
+        self.assertEqual( convert_csv_td.initialCsvFileCheck("unittest/transactions_2022csv"), False )
+        self.assertEqual( convert_csv_td.initialCsvFileCheck("unittest/transactions_2022.csvs"), False )
+        self.assertEqual( convert_csv_td.initialCsvFileCheck("unittest/transactions.csv_2022"), False )
+        self.assertEqual( convert_csv_td.initialCsvFileCheck("unittest/transactions_2022.csv"), True )
 
         # This should be true, but issue comes up in the next test case because it doesn't exist
         #self.assertEqual( convert_csv_td.initialCsvFileCheck("transactions_2022.csv")
 
     def test_input_exists( self ):
         # File doesn't exist
-        self.assertEqual( convert_csv_td.initialCsvFileCheck( "transactions_2022.csv" ), False )
-        self.assertEqual( convert_csv_td.initialCsvFileCheck("../../sensitive_files/transactions_2022.csv"), True )
+        self.assertEqual( convert_csv_td.initialCsvFileCheck("unittest/does_not_exist.csv" ), False )
+        self.assertEqual( convert_csv_td.initialCsvFileCheck("unittest/blank.csv"), False )
+        self.assertEqual( convert_csv_td.initialCsvFileCheck("unittest/transactions_2022.csv"), True )
 
 # TODO: Figure out how to structure unit code
-#class TestCsvContents( unittest.TestCase ):
-#    def test_contents_of_csv_check:
-#        self.assetEqual( convert_csv_td.contentsCsvFileCheck( ), )
-
+class TestCsvContents( unittest.TestCase ):
+    def test_contents_of_csv_check( self ):
+        self.assertEqual( convert_csv_td.contentsCsvFileCheck("unittest/no_rows.csv"), False )
+        self.assertEqual( convert_csv_td.contentsCsvFileCheck("unittest/one_row.csv"), True )
 
 
 if __name__ == "__main__":
