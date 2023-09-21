81b21cd91b46a2551632841e0aa6606a74cedacd
Parker Hooten <phooten@g.clemson.edu>
2023-09-20 23:35:51 -0600

    Added getters / setters and unit tests for everything up to this point. Created a function to get description cell and started working on getting expiration date for options.

--- tools/trade_history_to_csv/convert_td_csv/test/test_class_filter_csv.py
+++ tools/trade_history_to_csv/convert_td_csv/test/test_class_filter_csv.py
@@ -10,39 +10,133 @@ class TestClassFilterCsv( unittest.TestCase ):
     def setUpClass( cls ):
         print("\nsetUpClass method: Runs before all tests...")
 
+
     def setUp( self ):
         print("\nRunning setUp method...")
         self.filter = class_filter_csv.csvFilter()
 
+
     def tearDown(self):
         print("Running tearDown method...")
 
-    def test_set_output_row( self ):
-        # No checks currently. Will always return true
+
+    def test_setOutpuRow( self ):
+        """
+        Description:    Tests the setOuputRow member function. Currently there are no checks and will always return
+                        true.
+        """
         self.assertEqual( self.filter.setOutputRow(), True )
 
-    def test_format_date( self ):
+
+    #def test_filterDescriptionColumn( self ):
+        """
+        Description:    Tests checks for this function, but not for other member functions that alreaddy have unit
+                        tests. This would be redundant.
+        """
+        # TODO: Nothing to check right now.
+
+
+    def test_findDateOfAction(self):
+        """
+        Description:    Place holder, since it currently acts as a wrapper to formatDate(). This needs to be updated if
+                        the function ever changes.
+        """
+        # Bad Formats
+        dates = [ "001/31/2023",
+                  "1/31/20233" ] # Could keep going, but will be the same as test_formatDate()
+        for date in dates:
+            self.filter.mInputRow = date
+            self.assertEqual( self.filter.findDateOfAction(), False )
+
+        # Good format
+        self.filter.mInputRow = [ "01/31/2023" ]
+        self.assertEqual( self.filter.findDateOfAction(), True )
+
+
+    def test_formatDate( self ):
+        """
+        Description:    Tests the formatDate member function
+        """
+
         # Bad formats
         dates = [   "001/31/20233",
                     "1/31/20233",
+                    "01/001/2023",
                     "01/31/20233",
                     "01/31/202",
+                    "1/3/1",
                     "underline_here",
-                    "space here" ]
+                    "underline_goes_here",
+                    "dot.here",
+                    "dot.goes.here",
+                    "space here",
+                    "space goes here" ]
         for date in dates:
             self.assertEqual( self.filter.formatDate( date ), ( False, date ) )
 
-        # Good formats
-        dates = [   "01/31/2023",
-                    "Mar 12 2021" ]
+        # Good formats: Already in a good format
+        dates = [   "01/31/2023" ]
         for date in dates:
             self.assertEqual( self.filter.formatDate( date ), ( True, date ) )
 
+        # Good format: ( input, expected )
+        dates = [ ("Mar 12 2021", "03/12/2021"),
+                 ("Jan 29 1997", "01/29/1997") ]
+        for date in dates:
+            self.assertEqual( self.filter.formatDate( date[0] ), ( True, date[1] ) )
 
+
+    def test_getDescriptionCell( self ):
+        # Description column doesn't exist in mTdHeaders
+        self.filter.mTdHeaders = [  'DATE',
+                                    'TRANSACTION ID',
+                                    'ERROR_DESCRIPTION',
+                                    'QUANTITY',
+                                    'SYMBOL',
+                                    'PRICE',
+                                    'COMMISSION',
+                                    'AMOUNT',
+                                    'REG FEE',
+                                    'SHORT-TERM RDM FEE',
+                                    'FUND REDEMPTION FEE',
+                                    ' DEFERRED SALES CHARGE' ]
+        row = []
+        for num in range( len( self.filter.mTdHeaders ) ):
+            row.append( str( num ) )
+        self.filter.setInputRow( row )
+        self.assertEqual( self.filter.getDescriptionCell(), ( False, "ERROR" ) )
+
+        # Description column is past what is expected for input row
+        self.filter.mTdHeaders = [  'DATE',
+                                    'TRANSACTION ID',
+                                    'DESCRIPTION',
+                                    'QUANTITY',
+                                    'SYMBOL',
+                                    'PRICE',
+                                    'COMMISSION',
+                                    'AMOUNT',
+                                    'REG FEE',
+                                    'SHORT-TERM RDM FEE',
+                                    'FUND REDEMPTION FEE',
+                                    ' DEFERRED SALES CHARGE' ]
+        # Intensionally making row too short
+        row = [ "ONE", "TWO" ]
+        self.filter.setInputRow( row )
+        self.assertEqual( self.filter.getDescriptionCell(), ( False, "ERROR" ) )
+
+        # Correct Case
+        row = []
+        for num in range( len( self.filter.mTdHeaders ) ):
+            row.append( str( num ) )
+        self.filter.setInputRow( row )
+        # TODO: Don't like this hard coded
+        self.assertEqual( self.filter.getDescriptionCell(), ( True, row[ 2 ] ) )
     @classmethod
+
 
     def tearDownClass(cls):
         print("\ntearDownClass method: Runs after all tests...")
+
 
 if __name__ == "__main__":
     unittest.main()
--- tools/trade_history_to_csv/convert_td_csv/utility/class_filter_csv.py
+++ tools/trade_history_to_csv/convert_td_csv/utility/class_filter_csv.py
@@ -48,16 +48,16 @@ class csvFilter:
             self.mOutputRow.append( self.mNa )
 
         # Sets each value to default, then is put into the output row
-        self.mActionDate = self.mNa
-        self.mTicker = self.mNa
-        self.mType = self.mNa
-        self.mAction = self.mNa
-        self.mQuantity = self.mNa
-        self.mCost = self.mNa
-        self.mCommission = self.mNa
+        self._mActionDate = self.mNa
+        self._mTicker = self.mNa
+        self._mType = self.mNa
+        self._mAction = self.mNa
+        self._mQuantity = self.mNa
+        self._mCost = self.mNa
+        self._mCommission = self.mNa
         # Option specific
-        self.mExpDate = self.mNa
-        self.mStrike = self.mNa
+        self._mExpDate = self.mNa
+        self._mStrike = self.mNa
 
         # Sets output row to default values
         self.setOutputRow()
@@ -69,6 +69,7 @@ class csvFilter:
 
         return
 
+
     def setOutputRow( self ):
         """
         Description:    Sets the output row to cetain class value
@@ -77,16 +78,16 @@ class csvFilter:
         """
 
         # Makes List
-        self.mOutputRow[ 0 ] = self.mActionDate
-        self.mOutputRow[ 1 ] = self.mTicker
-        self.mOutputRow[ 2 ] = self.mType
-        self.mOutputRow[ 3 ] = self.mAction
-        self.mOutputRow[ 4 ] = self.mQuantity
-        self.mOutputRow[ 5 ] = self.mCost
-        self.mOutputRow[ 6 ] = self.mCommission
+        self.mOutputRow[ 0 ] = self.getActionDate()
+        self.mOutputRow[ 1 ] = self.getTicker()
+        self.mOutputRow[ 2 ] = self.getType()
+        self.mOutputRow[ 3 ] = self.getAction()
+        self.mOutputRow[ 4 ] = self.getQuantity()
+        self.mOutputRow[ 5 ] = self.getCost()
+        self.mOutputRow[ 6 ] = self.getCommission()
         # Option Specific
-        self.mOutputRow[ 7 ] = self.mExpDate
-        self.mOutputRow[ 8 ] = self.mStrike
+        self.mOutputRow[ 7 ] = self.getExpDate()
+        self.mOutputRow[ 8 ] = self.getStrike()
 
         return True
 
@@ -98,21 +99,22 @@ class csvFilter:
         """
 
         # Sets input row for the object to use
-        self.mInputRow = pRow
+        self.setInputRow( pRow )
 
         # Setting Dates
-        passed = self.setDateOfAction()
+        passed = self.findDateOfAction()
         if not passed:
             msg.error( __name__ + ": Couldn't set 'Date of Action.'", "TODO" )
             return False
 
-        # TODO: Write unit test code of all this before anything else
-        #   -   Set the experation date
+        passed = self.setDateOfExpiration()
+        if not passed:
+            msg.error( __name__ + ": Couldn't set 'Date of Expiration.'", "TODO" )
+            return False
 
         # Filters out description cell
         col_desc = self.mTdHeaders[ 2 ]
         print( "Target Cell: '" + str( pRow[ col_desc ] ) + "'" )
-
 
         # Sets the output row
         self.setOutputRow()
@@ -127,23 +129,39 @@ class csvFilter:
 
         return self.mOutputRow
 
-    def setDateOfAction( self, date="no_input" ):
+
+    def findDateOfAction( self ):
         """
-        Description:    Finds the date of action in the input row, then sets the member
+        Description:    Finds the date of action in the input row, then sets the member. Currently acts as a wrapper to
+                        formatDate, but it might change so should be left as is.
         Arguments:      date [ string ]:    Optional. Only used if user wants to override this date.
         Returns:        bool:   True for success, False for failure
         """
 
-        # User can override this value
-        if date != "no_input":
-            self.mActionDate = date
-            return True
-
         # Gets the row date and converts it to the correct format
+        # TODO: Get rid of the hard coded value
         passed, self.mActionDate = self.formatDate( self.mInputRow[ 0 ] )
         if not passed:
             msg.error( __name__ + ": Couldn't set 'Date of Action.'", "TODO" )
             return False
+
+        return True
+
+
+    def findDateOfExpiration( self ):
+        """
+        Description:    Finds the date of expiration in the input row, then sets the member.
+        Arguments:      date [ string ]:    Optional. Only used if user wants to override this date.
+        Returns:        bool:   True for success, False for failure
+        """
+
+        passed, description_cell = self.getDesciptionCell()
+        if not passed:
+            msg.system( "Couldn't find a description cell in the input.", "TODO" )
+            return False
+
+        # TODO: Write code to find the expiration date.
+
 
         return True
 
@@ -174,7 +192,6 @@ class csvFilter:
         # TODO: Don't like the hard coding
         # TODO: switch statement isn't supported in python?
         # Assign number for month
-        new_date = 'TMP'
         if month == 'Jan':
             new_date = '01'
         elif month == 'Feb':
@@ -201,7 +218,7 @@ class csvFilter:
             new_date = '12'
         else:
             msg.error( "date_list[0] not expected: " + month, "TODO" )
-            return False, new_date
+            return False, pDate
 
         # Formats days with leading 0
         if len(day) < 2:
@@ -214,7 +231,40 @@ class csvFilter:
         return True, new_date
 
 
-    def filterForOptions():
+    def getDescriptionCell( self ):
+        """
+        Description:    Finds the expected cell in the input row
+        Arguments:      none
+        Returns:        bool:   True for success, False for failure
+                        string: The input date but in the correct format
+        """
+
+        # Gets the position of the expected cell
+        position = 0
+        found = False
+        for header in self.mTdHeaders:
+            if header == "DESCRIPTION":
+                found = True
+                break
+            position += 1
+
+        # Couldn't find header
+        if not found:
+            msg.error( "Couldn't find 'DESCRIPTION' in TdHeader.", "TODO" )
+            return False, "ERROR"
+
+        # Position returned is greater than rows from intput
+        input_row = self.getInputRow()
+        if position > len( input_row ) - 1:
+            msg.error( "Description cell found at position '" + str( position ) + "', but last position of input row is '" + str( len( input_row ) - 1 ) + "'.", "TODO" )
+            return False, "ERROR"
+
+        description_cell = input_row[ position ]
+
+        return True, description_cell
+
+
+    def filterForOptions( self ):
         """
         Description:    Filters the following from the description:
                             *   Puts:   Buy / Sell / Assigned / Expired
@@ -225,8 +275,79 @@ class csvFilter:
 
         return
 
-    def filterForStocks():
+    def filterForStocks( self ):
         return
 
-    def filterForOther():
+    def filterForOther( self ):
+        return
+
+# Getters / Setters
+    def getActionDate( self ):
+        return self._mActionDate
+
+    def setActionDate( self, value ):
+        self._mActionDate = value
+        return
+
+    def getTicker( self ):
+        return self._mTicker
+
+    def setTicker( self, value ):
+        self._mTicker = value
+        return
+
+    def getType( self ):
+        return self._mType
+
+    def setType( self, value ):
+        self._mTicker = value
+        return
+
+    def getAction( self ):
+        return self._mAction
+
+    def setAction( self, value ):
+        self._mAction = value
+        reutrn
+
+    def getQuantity( self ):
+        return self._mQuantity
+
+    def setQuantity( self, value ):
+        self._mQuantity = value
+        return
+
+    def getCost( self ):
+        return self._mCost
+
+    def setCost( self, value ):
+        self._mCost = value
+        return
+
+    def getCommission( self ):
+        return self._mCommission
+
+    def setCommision( self, value ):
+        self._mCommission = value
+        return
+
+    def getExpDate( self ):
+        return self._mExpDate
+
+    def setExpDate( self, value ):
+        self._mExpDate = value
+        return
+
+    def getStrike( self ):
+        return self._mStrike
+
+    def setStrike( self, value ):
+        self._mStrike = value
+        return
+
+    def getInputRow( self ):
+        return self._mInputRow
+
+    def setInputRow( self, value ):
+        self._mInputRow = value
         return
