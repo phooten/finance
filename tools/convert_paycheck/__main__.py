f6c8af1e8857eea2a58f0a49a9769a5ddb7a0a3f
Parker Hooten <phooten@g.clemson.edu>
2023-10-03 23:38:01 -0600

    successfully filtered out all elements of a single pay check.

--- tools/convert_paycheck/__main__.py
+++ tools/convert_paycheck/__main__.py
@@ -15,6 +15,8 @@ This file is meant to automate moving my paycheck into an xml file.
 from dotenv import load_dotenv
 import pprint
 import pdfplumber
+import itertools
+import re
 
 # Environmental variables
 load_dotenv()
@@ -78,10 +80,14 @@ def ConvertPdfToText():
     Arguments:
     Returns:
     """
+
     # TODO: Fix this and the option-value-scan script. They are ahard coded values and
     #       Need to be replaced with environmental variables.
     REPO_PATH="/Users/phoot/code/finance/"
     PDF_PATH = str(REPO_PATH) + "sensitive_files/paystubs/test_statement.pdf"
+
+
+    # Breaks down PDF into two sections, then finds text based on that
     x0 = 0.00   # Distance of left side of character from left side of page.
     x1 = 0.54   # Distance of right side of character from left side of page.
     y0 = 0.00   # Distance of bottom of character from bottom of page.
@@ -106,7 +112,9 @@ def ConvertPdfToText():
             #if i < 2:  # help you see the merged first two pages
             #    print(page_context)
 
-    return all_content
+    content_list = page_context.split('\n')
+
+    return content_list
 
 
 
@@ -118,10 +126,18 @@ def RefineText( text_block ):
     """
 
     # These are hardcoded values we care about in the paycheck
-    keys = [ "Period Beginning",
+
+    # One argument after them
+    keys_one = [ 
+             "Period Beginning",
              "Period Ending",
              "Total Hours Worked",
              "Pay Rate",
+             "PTO Bank Balance",
+             "XXXXX1908" ]
+
+    # Two arguments after them. If missing one, 0
+    keys_two = [
              "Salary",
              "EIP",
              "2nd Shift Prem 1.0",
@@ -144,35 +160,98 @@ def RefineText( text_block ):
              "Imputed Income",
              "FED Taxable",
              "CO Taxable",
-             # These will be removed / not used
-             "Total Earnings",
-             "Net Pay",
-             "PTO Bank",
-             "Imputed Income",
-             "EMP Term Life",
-             "If you have any questions regarding your" ]
+             "Emp Term Life"
+              ]
 
-    # Adds an element for every single key
+    keys = keys_one + keys_two
+
+
+    # Adds an element for every key
     filtered_text = []
-    found = False
-    for curr in text_block:
-        for key in keys:
+    for key in keys:
+        found = False
+        for curr in text_block:
+
+            # Notes the element was found and adds to a new filtered block
             if key in curr:
                 found = True
                 filtered_text.append( curr )
-                keys.remove( key )
-                continue
 
+                # Removes both the key and the found element
+                text_block.remove( curr )
+                break
 
-    if len( keys ) != 0:
-        print( "Not every key was found. See keys: '" + str( keys ) + "'\n")
-        print( "Also see text:\n")
-        for curr in text_block:
-            print( curr )
+        # Error: Key not found
+        if not found:
+            print( "ERROR:" )
+            print( "Key not found '" + str( key ) + "'" )
+            print( "Text:\n")
+            for curr in text_block:
+                print( "\t" + curr )
+            exit(1)
+
+    # Error: Not every key is accounted for
+    if ( len( keys_one ) + len( keys_two ) ) != len( filtered_text ):
+        print( "ERROR:" )
+        print( "Not enough entries in fileted_text to match keys.")
+        print( "filted_text:" )
+        for curr in filtered_text: 
+            print( "\t" + str( curr ) )
+        print( "\nkeys_one:")
+        for curr in keys_one:
+            print( "\t" + str( curr ) )
+        print("")
+        print( "keys_two:" )
+        for curr in keys_two:
+            print( "\t" + str( curr ) )
+        print("")
         exit(1)
 
-    # Useful to see what the filtered text contains
-    pp.pprint( filtered_text )
+
+    # Filters out the one / two arguments
+    one_arg_list = []
+    two_arg_list = []
+    for curr in filtered_text:
+        for key in keys:
+            if key in curr:
+                tmp = curr.replace( key, '' )       # Removes keyword from text
+                tmp = tmp.replace( ':', '' )       # Removes all colons
+                tmp = re.sub(r'^\s+', '', tmp )     # Removes leading whitespace
+                tmp = re.sub(r'\s+$', '', tmp )     # Removes trailing whitespace
+                tmp = tmp.split( ' ' )
+                if ( len( tmp ) == 1 ) and ( key in keys_one ):
+                    one_arg_list.append( [ key, tmp[ 0 ] ] )
+                elif key in keys_two:
+                    if len( tmp ) == 1:
+                        two_arg_list.append( [ key, 0, tmp[ 0 ] ] )
+                    elif len( tmp ) == 2:
+                        two_arg_list.append( [ key, tmp[ 0 ], tmp[ 1 ] ] )
+                    else:
+                        print( "ERROR:" )
+                        print( "tmp length is '" + str( len ( tmp ) ) + "' but is in keys_two." )
+                        print( "key is '" + key + "'." )
+                        print( "" )
+                        exit(1)
+                else:
+                    print( "ERROR:" )
+                    print( "Current element doesn't have 1 or 2 arguments." )
+                    print( "len( tmp ) = " + str( len( tmp ) ) )
+                    print( "tmp = '" + str(tmp) + "'" )
+                    print( "curr: '" + curr + "'")
+                    exit( 1 )
+
+    print( "one_arg_list:" )
+    for curr in one_arg_list:
+        print( "\t" + str( curr ) )
+    print( 'two_arg_list' )
+    for curr in two_arg_list:
+        print( "\t" + str( curr ) )
+#
+#
+#    # Every key accounted for
+#    print( "\n\nFound all keys. See text:" )
+#    for curr in filtered_text:
+#        print( "\t" + str( curr ) )
 
     return filtered_text
 
