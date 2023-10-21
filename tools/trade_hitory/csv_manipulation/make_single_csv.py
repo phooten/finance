b518d7c97a6f5fb665434009033e06df7488be30
Parker Hooten <phooten@g.clemson.edu>
2023-10-21 17:08:12 -0600

    Updating user messages and functionality to create a single global csv file

--- tools/trade_hitory/csv_manipulation/make_single_csv.py
+++ tools/trade_hitory/csv_manipulation/make_single_csv.py
@@ -17,14 +17,14 @@ def makeOneGlobalCsv():
     global_transactions_file = transaction_path + "global_transactions.csv"
     global_transactions_file_backup = global_transactions_file + "_BACKUP.csv"
     all_sensitive_files = [ file for file in listdir(  transaction_path )if isfile( join( transaction_path, file ) ) ]
-    #print( __name__ + ": all files: " + str( onlyfiles ) )
+    #print( __name__ + ": all files: " + str( sensitive_files ) )
 
     converted_files = []
     pattern = re.compile( "^converted_transactions_20[0-9][0-9]\.csv$" )
     for file in all_sensitive_files:
         if pattern.match( str( file ) ):
             converted_files.append( file )
-            msg.system( "Found file to append '" + file + "'", __name__ )
+            msg.system( "Appending file to global csv '" + file + "'", __name__ )
 
     # Makes a backup of the global file if it exists, and removes the back up if it exists
     if os.path.exists( global_transactions_file_backup ):
@@ -50,7 +50,7 @@ def makeOneGlobalCsv():
 
     msg.system( "New file created under the name '" + global_transactions_file + "'", __name__ )
 
-    return global_transactions_file
+    return True, global_transactions_file
 
 
 def convertAllAvailableCsvFiles():
@@ -61,35 +61,50 @@ def convertAllAvailableCsvFiles():
     """
 
     mypath = "/Users/phoot/code/finance/sensitive_files"
-    onlyfiles = [ f for f in listdir( mypath ) if isfile( join( mypath, f ) ) ]
+    sensitive_files = [ f for f in listdir( mypath ) if isfile( join( mypath, f ) ) ]
 
     # TODO This needs to not be hard coded
     regex_pattern_transactions = "^transactions_20[0-9][0-9]\.csv$"
-    regex_pattern_transactions_converted = "^converted_transactions_20[0-9][0-9]\.csv$"
-
     pattern_transactions = re.compile( regex_pattern_transactions )
-    pattern_transactions_converted = re.compile( regex_pattern_transactions_converted )
+    files_to_convert = []
 
     # TODO: warn if already converted
     # TODO: Don't hard code single file conversion
-    for file in onlyfiles:
+    for file in sensitive_files:
         if pattern_transactions.match( file ):
-            print( "found a file to convert '" + file + "'" )
+            msg.system( "Found a file to convert '" + file + "'", __name__ )
+
+            # Checks the file isn't already converted
+            # TODO: Don't hard code this 'converted_'
+            converted_file = "converted_" + file
+            for file_conv in sensitive_files:
+                if file_conv == converted_file:
+                    msg.warning( "File has already been converted '" + file + "'. Waiting for user input.", __name__ )
+
+                    # Asks user if they want to continue
+                    user_input = ""
+                    while user_input != "y":
+                        user_input = input( "Would you like to overwrite 'conversion_" + file + "' ? (y/n): " )
+                        if user_input == "n":
+                            msg.warning( "User chose 'n' so script won't continue.", __name__ )
+                            return False
+                    msg.system( "User chose 'y' so '" + file + "' will be converted and overwite file. Script continuing.", __name__ )
+
+            # Adds file to list and will convert later
+            files_to_convert.append( file )
 
 
-    #    msg.error( "\nIncorrect file format: '" + args.input_file + "'\n"\
-    #               "See regex pattern:     '" + regex_pattern + "'\n",
-    #                __name__ )
-    #    return False, args.input_file
-    #file = ""
+    for file in files_to_convert:
+        msg.system( "Converting '" + file + "'", __name__ )
+        #python __main__.py -f file
+        try:
+            msg.system( "Running command: 'python convert_single_td_csv/__main__.py -f " + file + "'", __name__ )
+            os.system(f'python convert_single_td_csv/__main__.py -f {file}')
+        except FileNotFoundError:
+            # TODO: Don't hard code this
+            msg.error( "The python program 'convert_single_td_csv/__main__.py' does not exist.", __name__ )
 
-    # Find all the transaction files
-
-    # If converted file already, note that to operator
-
-    # If not, convert file and notify operator
-
-    return
+    return True
 
 
 def refineFile( raw_file ):
@@ -99,16 +114,45 @@ def refineFile( raw_file ):
     Returns:        Void
     """
 
-    #with 
+    count = 0
+    with open( raw_file ) as fileObject:
+        for row in fileObject:
+            if count < 10:
+                print( row )
 
-    return
+            if count == 0:
+                #TODO: need to remove the header in the middle of the file and renumber the first column
+                header = row
+                print( row )
+                print( header )
+
+            else:
+                if row == header:
+                    msg.error( "Found another header at count '" + str( count ) + "'.", __name__ )
+
+            count += 1
+
+    return True
 
 def main():
-    convertAllAvailableCsvFiles()
 
-    file = makeOneGlobalCsv()
+    passed = convertAllAvailableCsvFiles()
+    if not passed:
+        msg.error( "converAllAvailableCsvFiles() failed.", __name__ )
+        msg.quit_script()
 
-    refineFile( file )
+    passed, file = makeOneGlobalCsv()
+    if not passed:
+        msg.error( "makeOneGlobalCsv() failed.", __name__ )
+        msg.quit_script()
+        
+
+    passed = refineFile( file )
+    if not passed:
+        msg.error( "refineFile() failed.", __name__ )
+        msg.quit_script()
+
+    msg.quit_script()
 
     return
 
