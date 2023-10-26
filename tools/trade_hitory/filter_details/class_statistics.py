d15973d6755c72d6839301fe8e07d3ef6a6531c8
Parker Hooten <phooten@g.clemson.edu>
2023-10-25 22:37:08 -0600

    Starting the class to filter out what I want

---
+++ tools/trade_hitory/filter_details/class_statistics.py
@@ -0,0 +1,27 @@
+
+
+class statistics:
+
+    def readInCsv():
+        # Reads in a csv, then hangs onto the datafram within the class
+        return
+
+    def getTotalSharesOwned():
+        return
+
+    def getOptionsCompensation( year="", ticker="all", ):
+        """
+        Description:    
+        Arguments:      
+        Returns:        
+        """
+        # From the dataframe
+
+        
+
+        return
+
+    def getTotalCompensation():
+        # Total compensation - shares from the stocks
+        return
+
