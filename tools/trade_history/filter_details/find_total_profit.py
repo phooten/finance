ae4508d09b9585d1c3e3e4cffb99e2f2dfe58306
Parker Hooten <phooten@g.clemson.edu>
2024-03-08 13:53:02 -0700

    Addition of macro tool usage

--- tools/trade_history/filter_details/find_total_profit.py
+++ tools/trade_history/filter_details/find_total_profit.py
@@ -36,6 +36,7 @@ class UserInput():
         self.mDateRange = [ "01/01/2000", "01/01/2100" ]
         self.mType = [ ] # Blank List
         self.mOffset = 0.0
+        self.mMacro = ""
 # End of UserInput
 
 
@@ -46,7 +47,7 @@ GetUserInput
 
 @returns - Object that stores user selections
 """
-def GetUserInput( arguments ):
+def GetUserInput( arguments, dataframe ):
     # Default input object selection
     UserInputObj = UserInput()
 
@@ -61,9 +62,16 @@ def GetUserInput( arguments ):
 
     # Setting ticker list
     UserInputObj.mTickerList = list( arguments.ticker_list.split( " " ) )
-    UserInputObj.mTickerList = "TSLA".split( " " )
+    if "All" == UserInputObj.mTickerList[0]:
+        # Gets all tickers regaurdless of date selections
+        # Remove any tickers with symbols in it
+        UserInputObj.mTickerList = [ item for item in dataframe['TICKER'].unique() if not '-' in item ]
+
     # Setting Offset
     UserInputObj.mOffset = float( arguments.offset )
+
+    # Setting Offset
+    UserInputObj.mMacro = arguments.macro
 
     print( "Widget Input Details:")
     print( "_________________________________" )
@@ -72,6 +80,7 @@ def GetUserInput( arguments ):
     print( "Ticker List: " + str( UserInputObj.mTickerList ) )
     print( "Type:        " + str( UserInputObj.mType ) )
     print( "Offset:      " + str( UserInputObj.mOffset ) )
+    print( "Macro:       " + str( UserInputObj.mMacro ) )
     print( "\n\n" )
     return UserInputObj
 # End of GetUserInput
@@ -103,17 +112,18 @@ FilterDate
 
 @returns - CSV object with only items that falls between the date selection
 """
-def FilterDate( date_selection, csv_object ):
+def FilterDate( date_selection, dataframe ):
+    filtered_df = dataframe
     start_date = date_selection[0]
     end_date = date_selection[1]
 
     # Makes index the date of action so it can be filtered
-    #csv_object.set_index( "DATE OF ACTION", inplace = True )
+    #dataframe.set_index( "DATE OF ACTION", inplace = True )
 
     # Filters out the rows based on start / end date
-    #return csv_object.loc[ start_date:end_date ]
-    mask = ( csv_object[ 'DATE OF ACTION' ] > start_date) & ( csv_object[ 'DATE OF ACTION' ] <= end_date )
-    return csv_object.loc[ mask ]
+    #return dataframe.loc[ start_date:end_date ]
+    mask = ( filtered_df[ 'DATE OF ACTION' ] > start_date) & ( filtered_df[ 'DATE OF ACTION' ] <= end_date )
+    return filtered_df.loc[ mask ]
 # End of FilterDate
 
 
@@ -127,27 +137,30 @@ FilterType
 
 @returns - Returns CSV object with only the type that has the same type as the selection
 """
-def FilterType( type, csv_object ):
+def FilterType( type, dataframe ):
+    filtered_df = dataframe
     if type == "options":
-        csv_object = csv_object.loc[ ( csv_object[ 'TYPE' ] == "Put" ) | ( csv_object[ 'TYPE' ] == "Call" ) ]
+        filtered_df = filtered_df.loc[ ( filtered_df[ 'TYPE' ] == "Put" ) | ( filtered_df[ 'TYPE' ] == "Call" ) ]
     
     elif type == "stock":
-        csv_object = csv_object.loc[ ( csv_object[ 'TYPE' ] == "Stock" ) ]
+        filtered_df = filtered_df.loc[ ( filtered_df[ 'TYPE' ] == "Stock" ) ]
 
     elif type == "all":
         pass
         # Do nothing, no filtering
     else:
+        print( "Error: Unhandled FilterType")
+        exit( 1 )
         # NOTE: This might result in a blank csv if it's not found
-        try:
-            csv_object = csv_object.loc[ csv_object[ 'TYPE' ] == type ]
+        # try:
+        #     filtered_df = filtered_df.loc[ dataframe[ 'TYPE' ] == type ]
 
-        # TODO: Need to update this with the logger
-        except KeyError:
-            print( "Can't find type: '" + type + "'" )
-            exit( 1 )
+        # # TODO: Need to update this with the logger
+        # except KeyError:
+        #     print( "Can't find type: '" + type + "'" )
+        #     exit( 1 )
 
-    return csv_object
+    return filtered_df
 # End of Filter Type
 
 
@@ -161,24 +174,23 @@ FilterTicker
 
 @returns - 
 """
-def FilterTicker( ticker_list, csv_object ):
-    filtered_df = pd.DataFrame( columns = csv_object.columns )
-    for ticker in ticker_list:
-        try:
-            print( "Ticker: " + ticker )
-            curr_df = csv_object.loc[ csv_object[ 'TICKER' ] == ticker ]
-            filtered_df = pd.concat( [ filtered_df, curr_df ] )
+def FilterTicker( ticker, dataframe ):
+    filtered_df = pd.DataFrame( columns = dataframe.columns )
+    try:
+        # print( "Ticker: " + str( ticker.split( " " ) ) )
+        curr_df = dataframe.loc[ dataframe[ 'TICKER' ] == ticker ]
+        filtered_df = pd.concat( [ filtered_df, curr_df ] )
 
-        # TODO: Need to update this with the logger
-        except KeyError:
-            print( "Can't find ticker: '" + ticker + "'" )
-            exit( 1 )
+    # TODO: Need to update this with the logger
+    except KeyError:
+        print( "Can't find ticker: '" + ticker + "'" )
+        exit( 1 )
 
     return filtered_df
 # End of FilterTicker
 
 """
-SumAmountFloat
+SumAmount
 
 !@brief - 
 
@@ -187,16 +199,16 @@ SumAmountFloat
 
 @returns - 
 """
-def SumAmountFloat( csv_object ):
+def SumAmount( dataframe ):
     # TODO: Need to update this with the logger / add in a try / except
-    total_sum = csv_object[ "AMOUNT" ].astype( float ).sum( axis=0 )
+    total_sum = dataframe[ "AMOUNT" ].astype( float ).sum( axis=0 )
 
     return total_sum
-# End of SumAmountFloat
+# End of SumAmount
 
 
 """
-SumAmountInt
+SumQuantity
 
 !@brief - 
 
@@ -205,26 +217,35 @@ SumAmountInt
 
 @returns - 
 """
-def SumAmountInt( csv_object ):
+def SumQuantity( dataframe ):
     # TODO: Delete this, but really good to see all the dataframe
     # with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
     #     print( bought_df )
 
     # Drops all indexes not labeled "Bought" then sums the "QUANTITY" row
-    index_df = csv_object[ csv_object['ACTION'] != "Bought" ].index
-    bought_df = csv_object.drop( index_df )
+    index_df = dataframe[ dataframe['ACTION'] != "Bought" ].index
+    bought_df = dataframe.drop( index_df )
     sum_bought = bought_df[ 'QUANTITY' ].astype( int ).sum( axis=0 )
 
     # Drops all indexes not labeled "Sold" then sums the "QUANTITY" row
-    index_df = csv_object[ csv_object['ACTION'] != 'Sold' ].index
-    sold_df = csv_object.drop( index_df )
+    index_df = dataframe[ dataframe['ACTION'] != 'Sold' ].index
+    sold_df = dataframe.drop( index_df )
     sum_sold = sold_df[ 'QUANTITY' ].astype( int ).sum( axis=0 )
 
     # Prints out results
     difference = int( sum_bought - sum_sold )
 
     return difference
-# End of SumAmountInt
+# End of SumQuantity
+
+def SumDividend( dataframe ):
+    # Drops all indexes not labeled "Dividend" then sums the "QUANTITY" row
+    index_df = dataframe[ dataframe['ACTION'] != "Dividend" ].index
+    dividend_df = dataframe.drop( index_df )
+    dividend_total = dividend_df[ 'AMOUNT' ].astype( float ).sum( axis=0 )
+
+    return dividend_total
+# End of SumDividend
 
 def getArguments():
     parser = argparse.ArgumentParser( description='' )
@@ -268,7 +289,18 @@ def getArguments():
                         required=False,
                         default="Options",
                         type=str,
-                        help='The type which will define how the CSV is filtered.' )
+                        help='The type which will define how the CSV is filtered. Note: If \'all\' is selected,'\
+                             'all tickers existing in global transactions will be selected regaurdless of date,'\
+                             ' selection.' )
+
+    parser.add_argument( '--macro',
+                        choices=[ 'none', 'stock_report' ],
+                        metavar='Macro',
+                        dest='macro',
+                        required=False,
+                        default="none",
+                        type=str,
+                        help='Macros' )
 
     parser.add_argument( '-m',
                         help='available so we can debug in vscode' )
@@ -276,44 +308,122 @@ def getArguments():
     return parser.parse_args()
 # End of getArguments
 
+def BuildStockProfile( dataframe, ticker_list ):
+    status = True
+
+    # unique_tickers = dataframe['TICKER'].unique()
+    for ticker in ticker_list:
+        # Initializing values:
+        dividend_total = 0
+        options_premium = 0
+        stock_cost = 0
+        owned_shares = 0
+        cost_basis = 0
+        adjusted_cost_basis = 0
+
+        # Filtering out ticker
+        ticker_dataframe = FilterTicker( ticker, dataframe )
+
+        # Creating dataframes
+        stock_dataframe = FilterType( "stock", ticker_dataframe )
+        options_datafame = FilterType( "options", ticker_dataframe )
+
+        # Calculating values
+        dividend_total = SumDividend( stock_dataframe )
+        options_premium = SumAmount( options_datafame )
+        stock_cost = SumAmount( stock_dataframe )
+        owned_shares = SumQuantity( stock_dataframe )
+
+        if 0 < owned_shares:
+            if stock_cost <= 0:
+                cost_basis = abs( stock_cost ) / owned_shares
+                adjusted_cost_basis = ( abs( stock_cost ) - options_premium ) / owned_shares
+            else:
+                # TODO: Use logger
+                print( "Error: Stock cost non-negative" )
+                exit( 1 )
+
+        elif 0 > owned_shares:
+            # TODO: Use logger
+            print( "Error: can't have negative shares" )
+            exit( 1 )
+
+
+        print( "" )
+        print( "==== " + str( ticker ) + " ===========================" )
+        print( "Owned shares:       " + str( owned_shares ) )
+        print( "Dividend total:     ${:.2f}".format( dividend_total ) )
+        print( "Options Premium:    ${:.2f}".format( options_premium ) )
+        if owned_shares != 0:
+            print( "Stock cost:         ${:.2f}".format( stock_cost ) )
+            print( "Cost Basis:         ${:.2f}".format( cost_basis ) )
+            print( "Cost basis ( adj ): ${:.2f}".format( adjusted_cost_basis ) )
+        else:
+            print( "Total Profit:       ${:.2f}".format( stock_cost ) )
+            print( " -- No costbasis. No shares owned. --" )
+
+
+    return status
+# End of BuildStock Profile
+
 def main():
     # Gets arguments from user
     args = getArguments()
-
-    # Gets the user input of what the csv should filter
-    UserInputObj = GetUserInput( args )
 
     # TODO: Vscode isn't finding this path while debugging...
     # Turns the master trade history into a pandas data frame to work with
     df = pd.read_csv( GetMasterCsv(), sep=',' )
 
-    # Filters based on ticker
-    if len( UserInputObj.mTickerList ) != 0:
-        df = FilterTicker( UserInputObj.mTickerList, df )
+    # Gets the user input of what the csv should filter
+    UserInputObj = GetUserInput( args, df )
 
-    # Filters based on date
-    df = FilterDate( UserInputObj.mDateRange, df )
+    if "stock_report" == UserInputObj.mMacro:
+        if not BuildStockProfile( df, UserInputObj.mTickerList ):
+            # TODO: Replace with logger
+            print( "Error: issue building stock profile" )
+            exit( 1 )
 
-    # Filter based on type
-    df = FilterType( UserInputObj.mType, df )
+    elif "funding" == UserInputObj.mMacro:
+        if True:
+            # TODO: Replace with logger
+            print( "Error: add function to calculate funding" )
+            exit( 1 )
 
-    # TODO: This works for options. What about Stocks?
-    # TODO: Think I want to put this into it's own function to handle tha outputs / final results
-    # Prints out the total sum of filtered dataframe
-    print( df )
-    total_cost = round( SumAmountFloat( df ), 2 ) + float( UserInputObj.mOffset )
-    print( "Offset Entered: $" + str( UserInputObj.mOffset ) )
-    print( "Total Cost:     $" + str( total_cost ) )
+    elif "none" == UserInputObj.mMacro:
+        # TODO: Clean this up, maybe move into a function
+        # Filters based on ticker
+        if len( UserInputObj.mTickerList ) != 0:
+            for ticker in UserInputObj.mTickerList:
+                df = FilterTicker( ticker, df )
 
-    if UserInputObj.mType == "Stock":
-        total_amount = SumAmountInt( df )
-        print( "Total Amount:    " + str( total_amount ) )
+        # Filters based on date
+        df = FilterDate( UserInputObj.mDateRange, df )
 
-        if total_amount != 0:
-            cost_per_share = abs( round( ( total_cost / total_amount ), 2 ) )
-            print( "Cost per share:  $" + str( cost_per_share ) )
-        else:
-            cost_per_share = "[ No shares owned. ]"
+        # Filter based on type
+        df = FilterType( UserInputObj.mType, df )
+
+        # TODO: This works for options. What about Stocks?
+        # TODO: Think I want to put this into it's own function to handle tha outputs / final results
+        # Prints out the total sum of filtered dataframe
+        print( df )
+        total_cost = round( SumAmount( df ), 2 ) + float( UserInputObj.mOffset )
+        print( "Offset Entered: $" + str( UserInputObj.mOffset ) )
+        print( "Total Cost:     $" + str( total_cost ) )
+
+        if UserInputObj.mType == "Stock":
+            total_amount = SumQuantity( df )
+            print( "Total Amount:    " + str( total_amount ) )
+
+            if total_amount != 0:
+                cost_per_share = abs( round( ( total_cost / total_amount ), 2 ) )
+                print( "Cost per share:  $" + str( cost_per_share ) )
+            else:
+                cost_per_share = "[ No shares owned. ]"
+    else:
+        # TODO: Replace with logger
+        print( "Error: Unhandled macro choice" )
+        exit( 1 )
+    # End macro choice
 
 
     return
